1750d3c4711fc1b3316792f03e2a1f41d447c336
Solomon <solomonaz2022@gmail.com>
2023-10-02 17:04:31 +0300

    chatting app Ui added

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from authentication.models import Account
 
 # Create your views here.
 def index(request):
-    return render(request, 'chat/index.html')
+    users = Account.objects.all()
+    context = {
+        'users':users,
+    }
+    return render(request, 'chat/index.html', context)

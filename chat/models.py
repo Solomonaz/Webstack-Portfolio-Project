c1259442cca8513b3155ccd7fc542b152574c46c
Solomon <Solomoaz2022@gmail.comn>
2023-10-08 20:06:07 +0300

    chat

--- chat/models.py
+++ chat/models.py
@@ -13,3 +13,14 @@ class Message(models.Model):
     def __str__(self):
         return f'{self.sender.username} - {self.timestamp}'
 
+class SentMessageInfo(models.Model):
+    sender_content = models.TextField()
+    sender_timestamp = models.DateTimeField(auto_now_add=True)
+    sender_info = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='sent_message')
+    receiver_info = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='received_message')
+
+    class Meta:
+        ordering = ['sender_timestamp']
+
+    def __str__(self):
+        return f'{self.sender_info.username} - {self.sender_timestamp}'
--- chat/views.py
+++ chat/views.py
@@ -4,67 +4,40 @@ from django.shortcuts import get_object_or_404
 from .forms import MessageForm
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from .models import Message
+from .models import Message, SentMessageInfo
 
 @login_required
 def index(request):
-    users = Account.objects.exclude(id=request.user.id)
-    
+    users = Account.objects.exclude(id=request.user.id) 
     message_data = Message.objects.all()
-    
-    receiver = request.user
-    
-    messages = Message.objects.filter(
-        (Q(sender=request.user, receiver=receiver) | Q(sender=receiver, receiver=request.user))
-    ).order_by('timestamp')
-    
-    if request.method == 'POST':
-        message_form = MessageForm(request.POST)
-        if message_form.is_valid():
-            message = message_form.save(commit=False)
-            message.sender = request.user
-            message.receiver = receiver
-            message.save()
-            return redirect('chat:index')  # Adjust the namespace and URL name if needed
-    
-    else:
-        message_form = MessageForm()
-    
+ 
     context = {
         'users': users,
         'message_data': message_data,
-        'receiver': receiver,
-        'messages': messages,
-        'message_form': message_form
     }
     
     return render(request, 'chat/index.html', context)
 
+@login_required
+def direct_message_detail(request, user_id):
+    receiver = get_object_or_404(Account, id=user_id)
+    # receiver = request.user
+    messages = Message.objects.filter(
+        (Q(sender=request.user, receiver=receiver) | Q(sender=receiver, receiver=request.user))
+    ).order_by('timestamp')
 
-def chat_room(request, room_name):
-    return render(request, 'chat.html', {'room_name': room_name})
-
-
-
-
-# @login_required
-# def direct_message_detail(request, user_id):
-#     receiver = get_object_or_404(Account, id=user_id)
-#     messages = Message.objects.filter(
-#         (Q(sender=request.user, receiver=receiver) | Q(sender=receiver, receiver=request.user))
-#     ).order_by('timestamp')
-
-#     if request.method == 'POST':
-#         message_form = MessageForm(request.POST)
-#         print(message_form)
-#         if message_form.is_valid():
-#             message = message_form.save(commit=False)
-#             message.sender = request.user
-#             message.receiver = receiver
-#             message.save()
-#             return redirect('index', user_id=user_id)
-#     else:
-#         message_form = MessageForm()
-
-#     context = {'receiver': receiver, 'messages': messages, 'message_form': message_form}
-#     return render(request, 'chat/index.html', context)
+    if request.method == 'POST':
+        message_form = MessageForm(request.POST)
+        if message_form.is_valid():
+            new_data = SentMessageInfo(
+                sender_content = message_form.content,
+                sender_timestamp = Message.timestamp,
+                sender_info  = request.user,
+                receiver_info = receiver,
+            )
+            new_data.save()
+            return redirect('index/')  
+    context = {
+        'message_form':message_form,
+    }          
+    return render(request, 'chat/index.html', context)
